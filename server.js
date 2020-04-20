e94d49c5691e51fab764bb541696280f06a77f74
tboyd4 <tboyd1898@gmail.com>
2020-04-20 16:34:44 -0500

    added import for axios

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const events = require('./db/events.json')
+const axios = require('axios')
 
 const app = express()
 const PORT = process.env.PORT || 3001
