dad41bcacdb24a95958ecffe59ab533405bdf2f9
tboyd4 <tboyd1898@gmail.com>
2020-04-20 14:24:26 -0500

    adding html for deployment to heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+var path = require('path');
 
 const PORT = process.env.PORT || 3030;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('I am a server')
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT, () => {
